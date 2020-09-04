7241b754e9969cc6babef0f8696f3bee696b7740
Dan S. Myers <dmyers@rollins.edu>
2020-09-04 10:59:04 -0400

    Update SwapExample.java

--- Sprint-1-Intro_to_Java/Examples/2-Variables/SwapExample.java
+++ Sprint-1-Intro_to_Java/Examples/2-Variables/SwapExample.java
@@ -1,16 +1,16 @@
-// Example of exchanging two variables using a temporary value
-// CMS 167
+// Exchanging two variables using a temporary value
+// CMS 121
 
 public class SwapExample {
 
-  public static void main(String[] args) {
-    int firstValue = 1000;
-    int secondValue = 1;
+    public static void main(String[] args) {
+        int firstValue = 1000;
+        int secondValue = 1;
     
-    int temp = firstValue;
+        int temp = firstValue;
     
-    firstValue = secondValue;
-    secondValue = temp;
-  }
+        firstValue = secondValue;
+        secondValue = temp;
+    }
   
 }
